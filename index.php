e8d77c6bc894853a15e2a7c9bc77adaddafeb4e2
juan_sanz <juan_sanz@estudante.sesisenai.org.br>
2025-09-17 12:56:00 -0300

    UPDATE - Fiz mais uma parte do login

--- index.php
+++ index.php
@@ -9,29 +9,14 @@ if(isset($POST['email']) || isset($_POST['senha'])) {
     } else if(strlen($_POST['senha']) == 0) {
         echo "Preencha sua senha";
     } else {
+        $nome = $_POST['username'];
+        $senha = $_POST['password'];
 
-        $email = $mysqli->real_escape_string($_POST['email']);
-        $senha = $mysqli->real_escape_string($_POST['senha']);
-
-        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
-        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
-
-        $quantidade = $sql_query->num_rows;
-
-        if($quantidade == 1) {
-            $usuario = $sql_query->fetch_assoc();
-
-            if(!isset($_SESSION)) {
-                    session_start();
-            }
-
-            $_SESSION['user'] = $usuario['id'];
-            $_SESSION['nome'] = $usuario['nome'];
-
-            header("Location: painel.php");
-
-        } else {
-            echo "Falha ao logar! E-mail ou senha incorretos";
+        $stmt = $mysqli->prepare("SELECT usuario_id, nome, hash_senha FROM usuarios WHERE nome = ?, hash_senha = ?");
+        $stmt->bind_param("ss", $nome, $senha);
+        if($stmt->execute()){
+            echo "Login feito";
+            header("location: dashboard.php");
         }
     }
 }
